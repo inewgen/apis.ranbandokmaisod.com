12c003ef4b24d205531f6bebd3b0c4e238ccfd1f
Suraches Seesat <suraches.see@ascendcorp.com>
2017-04-03 01:26:05 +0700

    update

--- app/controllers/ShopSettingsController.php
+++ app/controllers/ShopSettingsController.php
@@ -117,7 +117,7 @@ class ShopSettingsController extends ApiController
         $filters = array(
             'id' => $id,
         );
-        $query = Banners::filters($filters)
+        $query = ShopSettings::filters($filters)
             ->with('images')->get();
         $results = json_decode($query, true);
 
@@ -127,35 +127,9 @@ class ShopSettingsController extends ApiController
             return API::createResponse($response, 1004);
         }
 
-        //Loop data
-        $entries = array();
-        if (isset($results) && is_array($results)) {
-            foreach ($results as $key => $value) {
-                $entry = array();
-                if (isset($value) && is_array($value)) {
-                    $user_id = array_get($value, 'id', 0);
-                    foreach ($value as $key2 => $value2) {
-                        if ($key2 == 'images') {
-                            // Loop images
-                            $images = loopImages($value2, $user_id);
-                            $entry[$key2] = $images;
-                        } else if ($key2 == 'categories') {
-                            $category['id'] = $value2['id'];
-                            $category['title'] = $value2['title'];
-                            $entry[$key2] = $category;
-                        } else {
-                            $entry[$key2] = $value2;
-                        }
-                    }
-                }
-                $entries[] = $entry;
-            }
-        }
-
-        $entries  = $entries[0];
         $response = array(
             'cached' => false,
-            'record' => $entries
+            'record' => $results
         );
 
         // Save cache value
@@ -170,16 +144,10 @@ class ShopSettingsController extends ApiController
 
         // Validator request
         $rules = array(
-            'user_id' => 'required|integer|min:1',
-            'title' => 'min:1|max:255',
-            'subtitle' => 'min:1|max:255',
-            'button' => 'integer|min:1|in:0,1',
-            'button_title' => 'min:1|max:50',
-            'button_url' => 'min:1|max:150',
-            'images' => 'required',
+            'key'      => 'required|integer|min:1',
+            'value'    => '',
             'position' => 'integer|max:2',
-            'type' => 'integer|min:1',
-            'status' => 'integer|min:1|in:0,1',
+            'status'   => 'integer|min:1|in:0,1',
         );
 
         $validator = Validator::make($data, $rules);
@@ -192,23 +160,16 @@ class ShopSettingsController extends ApiController
         }
 
         $parameters = array(
-            'user_id' => $data['user_id'],
-            'title' => (isset($data['title']) ? $data['title'] : ''),
-            'subtitle' => (isset($data['subtitle']) ? $data['subtitle'] : ''),
-            'button' => (isset($data['button']) ? $data['button'] : '1'),
-            'button_title' => (isset($data['button_title']) ? $data['button_title'] : ''),
-            'button_url' => (isset($data['button_url']) ? $data['button_url'] : ''),
+            'key'      => (isset($data['key']) ? $data['key'] : ''),
+            'value'    => (isset($data['value']) ? $data['value'] : ''),
             'position' => (isset($data['position']) ? $data['position'] : '0'),
-            'type' => (isset($data['type']) ? $data['type'] : '1'),
-            'status' => (isset($data['status']) ? $data['status'] : '1'),
-            'updated_at' => "0000-00-00 00:00:00",
-            'created_at' => date("Y-m-d H:i:s"),
+            'status'   => (isset($data['status']) ? $data['status'] : '1'),
         );
 
         // [1] Insert
         $query = false;
         if (isset($parameters) && is_array($parameters) && (count($parameters) > 0)) {
-            $query = new Banners();
+            $query = new ShopSettings();
             foreach ($parameters as $key => $value) {
                 $query->$key = $value;
             }
@@ -222,17 +183,6 @@ class ShopSettingsController extends ApiController
         }
 
         $id = (isset($query->id) ? $query->id : null);
-
-        // Insert images
-        if ($images = array_get($data, 'images', false)) {
-            foreach ($images as $key => $value) {
-                $image = self::insertImageable($value, $id, 'banners');
-
-                if (!$image) {
-                    return API::createResponse('Error, Insert image', 1001);
-                }
-            }
-        }
 
         $response = array(
             'data' => $data,
@@ -321,7 +271,7 @@ class ShopSettingsController extends ApiController
             }
         }
 
-        $query = Banners::where('id', '=', $id)
+        $query = ShopSettings::where('id', '=', $id)
             ->update($banner);
 
         if (!$query) {
@@ -359,38 +309,13 @@ class ShopSettingsController extends ApiController
             return API::createResponse($response, 1003);
         }
 
-        $query = Banners::find($id);
+        $query = ShopSettings::find($id);
         $query->delete();
 
         if (!$query) {
             $response = array();
 
             return API::createResponse($response, 1004);
-        }
-
-        // Delete images
-        if ($images_id = array_get($data, 'images_id', false)) {
-            // Delete imageables
-            $filters = array(
-                'images_id' => $images_id,
-                'imageable_id' => $id,
-                'imageable_type' => 'banners',
-            );
-
-            $query_ia = Imageables::filters($filters);
-            if ($query_ia) {
-                $query_ia->delete();
-            }
-
-            // Delete image
-            $filters = array(
-                'id' => $images_id,
-            );
-
-            $query_i = Images::filters($filters);
-            if ($query_i) {
-                $query_i->delete();
-            }
         }
 
         $response = array(
@@ -401,31 +326,5 @@ class ShopSettingsController extends ApiController
         clearCacheDestroy($this->pathcache, $id);
 
         return API::createResponse($response, 0);
-    }
-
-    private function insertImageable($image_id = null, $banner_id = null, $section = null)
-    {
-        if (!isset($image_id) || !isset($banner_id) || !isset($section)) {
-            return false;
-        }
-
-        // Insert imageables
-        $parameters = array(
-            'images_id' => $image_id,
-            'imageable_id' => $banner_id,
-            'imageable_type' => $section,
-        );
-
-        $query = new Imageables();
-        foreach ($parameters as $key => $value) {
-            $query->$key = $value;
-        }
-        $query->save();
-
-        if (!$query) {
-            return false;
-        }
-
-        return true;
     }
 }
